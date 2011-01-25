2db6874ab28dae001e50a9414e31a67f51d1924b
melix <cedric.champeau@gmail.com>
2011-01-25 14:49:28 +0100

    Implemented caching in order to improve performance

--- src/org/codenarc/idea/CodeNarcInspectionTool.java
+++ src/org/codenarc/idea/CodeNarcInspectionTool.java
@@ -31,11 +31,14 @@ import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.util.Key;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiErrorElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiUtil;
+import com.intellij.psi.util.*;
 import org.codenarc.rule.Rule;
 import org.codenarc.rule.Violation;
+import org.codenarc.source.SourceCode;
 import org.codenarc.source.SourceString;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
@@ -51,6 +54,9 @@ import java.util.ResourceBundle;
  */
 public abstract class CodeNarcInspectionTool extends LocalInspectionTool {
     private static final String GROUP_DISPLAY_NAME = "CodeNarc";
+    private static final Key<CachedValue<SourceString>> SOURCE_AS_STRING_CACHE_KEY = Key.<CachedValue<SourceString>>create("CODENARC_SOURCE_AS_STRING");
+    private static final Key<CachedValue<Boolean>> HAS_SYNTAX_ERRORS_CACHE_KEY = Key.<CachedValue<Boolean>>create("CODENARC_HAS_SYNTAX_ERRORS");
+
     private ResourceBundle bundle;
 
     private String shortName;
@@ -123,26 +129,48 @@ public abstract class CodeNarcInspectionTool extends LocalInspectionTool {
     @Override
     public ProblemDescriptor[] checkFile(@NotNull final PsiFile file, @NotNull final InspectionManager manager, final boolean isOnTheFly) {
         if (file.getFileType().getName().equalsIgnoreCase("groovy")) {
-            final Editor editor = PsiUtil.findEditor(file);
-            final List<ProblemDescriptor> descriptors = new LinkedList<ProblemDescriptor>();
-            try {
-                final List<Violation> list = rule.applyTo(new SourceString(file.getText()));
+            CachedValue<Boolean> hasErrorsCachedValue = file.getUserData(HAS_SYNTAX_ERRORS_CACHE_KEY);
+            if (hasErrorsCachedValue==null) {
+                final CachedValuesManager cachedValuesManager = CachedValuesManager.getManager(manager.getProject());
+                hasErrorsCachedValue = cachedValuesManager.createCachedValue(new CachedValueProvider<Boolean>() {
+                    public Result<Boolean> compute() {
+                        PsiErrorElement errorElement = PsiTreeUtil.findChildOfType(file, PsiErrorElement.class);
+                        return Result.create(errorElement!=null, file);
+                    }
+                }, false);
+            }
+
+            if (!hasErrorsCachedValue.getValue()) { // avoid inspection if any syntax error is found
+                final Editor editor = PsiUtil.findEditor(file);
+                final List<ProblemDescriptor> descriptors = new LinkedList<ProblemDescriptor>();
+                CachedValue<SourceString> sourceStringCachedValue = file.getUserData(SOURCE_AS_STRING_CACHE_KEY);
+                if (sourceStringCachedValue==null) {
+                    final CachedValuesManager cachedValuesManager = CachedValuesManager.getManager(manager.getProject());
+                    sourceStringCachedValue = cachedValuesManager.createCachedValue(new CachedValueProvider<SourceString>() {
+                        public Result<SourceString> compute() {
+                            return Result.create(new SourceString(file.getText()), file);
+                        }
+                    }, false);
+                    file.putUserData(SOURCE_AS_STRING_CACHE_KEY, sourceStringCachedValue);
+                }
+                SourceCode code = sourceStringCachedValue.getValue();
+                final List<Violation> list = rule.applyTo(code);
                 for (final Violation violation : list) {
                     final int startOffset = editor.getDocument().getLineStartOffset(violation.getLineNumber() - 1);
                     final String message = violation.getMessage();
                     final PsiElement element = PsiUtil.getElementAtOffset(file, startOffset);
                     ProblemDescriptor descriptor = manager.createProblemDescriptor(
                             element,
-                            message == null ? description==null?rule.getName():description : message,
+                            message == null ? description == null ? rule.getName() : description : message,
                             ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
                             null,
                             isOnTheFly);
                     descriptors.add(descriptor);
                 }
-            } catch (Throwable e) {
-                /* Probably a syntax error */
+                return descriptors.toArray(new ProblemDescriptor[descriptors.size()]);
+            } else {
+                return null;
             }
-            return descriptors.toArray(new ProblemDescriptor[descriptors.size()]);
         } else {
             return null;
         }
